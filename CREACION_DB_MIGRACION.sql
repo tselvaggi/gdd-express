75318abb4c4cc524af36e6cc7512ab637b373a2e
tselvaggi <selvaggitomas@gmail.com>
2022-06-04 01:58:08 -0300

    Escuderias, Autos y Pilotos

--- CREACION_DB_MIGRACION.sql
+++ CREACION_DB_MIGRACION.sql
@@ -94,14 +94,14 @@ IF OBJECT_ID('GDD_EXPRESS.Pais', 'U') IS NOT NULL
 
 CREATE TABLE GDD_EXPRESS.Pais
 (
-PAIS_ID					int, --PK
+PAIS_ID					int IDENTITY(1,1), --PK
 PAIS_DETALLE				nvarchar(255),
 PRIMARY KEY (PAIS_ID)
 )
 
 CREATE TABLE GDD_EXPRESS.Escuderia
 (
-ESCUDERIA_ID				int, --PK
+ESCUDERIA_ID				int IDENTITY(1,1), --PK
 ESCUDERIA_NOMBRE			nvarchar(255),
 ESCUDERIA_PAIS_ID			int,
 PRIMARY KEY(ESCUDERIA_ID),
@@ -111,17 +111,18 @@ FOREIGN KEY (ESCUDERIA_PAIS_ID) REFERENCES GDD_EXPRESS.Pais (PAIS_ID)
 
 CREATE TABLE GDD_EXPRESS.Auto
 (
-AUTO_ID					int , --PK
+AUTO_ID					int IDENTITY(1,1), --PK
 AUTO_MODELO				nvarchar(255),
 AUTO_ESCUDERIA_ID			int, --FK
+AUTO_NUMERO					int,
 PRIMARY KEY (AUTO_ID),
 FOREIGN KEY (AUTO_ESCUDERIA_ID) REFERENCES GDD_EXPRESS.Escuderia (ESCUDERIA_ID)
 )
 
 CREATE TABLE GDD_EXPRESS.Piloto
 (
-PILOTO_ID				int, --PK
-PILOTO_AUTO_ID				int, --FK
+PILOTO_ID				int IDENTITY(1,1), --PK
+PILOTO_AUTO_ID				int UNIQUE, --FK
 PILOTO_NOMBRE				nvarchar(50),
 PILOTO_APELLIDO				nvarchar(50),
 PILOTO_NACIONALIDAD			int,
@@ -369,3 +370,259 @@ FOREIGN KEY (TELE_ID) REFERENCES GDD_EXPRESS.Telemetria (TELE_ID),
 FOREIGN KEY (NEUMATICO_ID) REFERENCES GDD_EXPRESS.Neumatico (NEUMATICO_ID)
 )	
 
+
+
+-- STORE PROCEDURES --
+
+IF OBJECT_ID('GDD_EXPRESS.migracion_parametros', 'P') IS NOT NULL
+    DROP PROCEDURE GDD_EXPRESS.migracion_parametros
+GO
+
+GO
+CREATE PROCEDURE GDD_EXPRESS.migracion_parametros
+AS
+BEGIN
+
+	BEGIN TRY
+		BEGIN TRANSACTION
+			--TABLA PAISES/NACIONALIDADES
+			INSERT INTO GDD_EXPRESS.Pais (PAIS_DETALLE)
+			SELECT distinct m.CIRCUITO_PAIS FROM gd_esquema.Maestra m
+			where m.CIRCUITO_PAIS IS NOT NULL
+			UNION
+			SELECT distinct m.ESCUDERIA_NACIONALIDAD FROM gd_esquema.Maestra m
+			where m.ESCUDERIA_NACIONALIDAD IS NOT NULL
+			UNION
+			SELECT distinct m.PILOTO_NACIONALIDAD FROM gd_esquema.Maestra m	
+			where  m.PILOTO_NACIONALIDAD IS NOT NULL
+
+			--TABLA TIPO SECTOR
+			INSERT INTO GDD_EXPRESS.Sector_Tipo (SECTOR_TIPO_DETALLE)
+			SELECT distinct m.SECTO_TIPO FROM gd_esquema.Maestra m
+			where NOT m.SECTO_TIPO IS NULL 
+
+			--TABLA CLIMA
+			INSERT INTO GDD_EXPRESS.Clima (CLIMA_DETALLE)
+			SELECT distinct m.CARRERA_CLIMA FROM gd_esquema.Maestra m
+			where NOT m.CARRERA_CLIMA IS NULL 
+
+
+		
+		COMMIT TRANSACTION	
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		DECLARE @errorDescripcion VARCHAR(255)
+		SELECT @errorDescripcion = ERROR_MESSAGE() + ' ERROR MIGRANDO DATOS EN TABLAS PARAMETROS';
+        THROW 50002, @errorDescripcion, 1
+	END CATCH
+		
+END
+GO  
+
+
+IF OBJECT_ID('GDD_EXPRESS.migracion_autos_escuderias', 'P') IS NOT NULL
+    DROP PROCEDURE GDD_EXPRESS.migracion_autos_escuderias
+GO
+
+GO
+CREATE PROCEDURE GDD_EXPRESS.migracion_autos_escuderias
+AS
+BEGIN
+	DECLARE c_escuderia CURSOR FOR
+		SELECT distinct ESCUDERIA_NOMBRE, ESCUDERIA_NACIONALIDAD FROM gd_esquema.Maestra
+		where ESCUDERIA_NOMBRE IS NOT NULL
+		
+	
+	CREATE TABLE #t_autos(
+		AUTO_MODELO nvarchar(255),
+		AUTO_NUMERO int,
+		ESCUDERIA_NOMBRE nvarchar(255)
+	)
+
+	BEGIN TRY
+	BEGIN TRANSACTION
+
+		INSERT INTO #t_autos
+			SELECT distinct m.AUTO_MODELO, m.AUTO_NUMERO, m.ESCUDERIA_NOMBRE FROM gd_esquema.Maestra m
+			where m.AUTO_MODELO IS NOT NULL
+
+		declare @escuderia_nombre nvarchar(255)
+		declare @escuderia_nacionalidad nvarchar(255)
+
+		OPEN c_escuderia
+		FETCH NEXT FROM c_escuderia INTO @escuderia_nombre, @escuderia_nacionalidad
+		WHILE (@@FETCH_STATUS = 0)
+		BEGIN
+			declare @id_pais int
+			declare @id_escuderia int
+			declare @id_auto int
+
+			SET @id_pais = (select p.PAIS_ID from GDD_EXPRESS.Pais p WHERE p.PAIS_DETALLE = @escuderia_nacionalidad)
+
+			INSERT INTO GDD_EXPRESS.Escuderia (ESCUDERIA_NOMBRE, ESCUDERIA_PAIS_ID)
+			VALUES (@escuderia_nombre, @id_pais)
+
+			SET @id_escuderia = @@IDENTITY
+
+			INSERT INTO GDD_EXPRESS.Auto (AUTO_MODELO, AUTO_NUMERO, AUTO_ESCUDERIA_ID)
+				SELECT ta.AUTO_MODELO, ta.AUTO_NUMERO, @id_escuderia FROM #t_autos ta
+				WHERE ta.ESCUDERIA_NOMBRE = (SELECT esc.ESCUDERIA_NOMBRE FROM GDD_EXPRESS.Escuderia esc
+				WHERE esc.ESCUDERIA_ID = @id_escuderia)
+
+			FETCH NEXT FROM c_escuderia INTO @escuderia_nombre, @escuderia_nacionalidad
+
+		END
+
+		CLOSE c_escuderia  
+		DEALLOCATE c_escuderia 
+	COMMIT TRANSACTION	
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		DECLARE @errorDescripcion VARCHAR(255)
+		SELECT @errorDescripcion = ERROR_MESSAGE() + ' ERROR MIGRANDO DATOS EN TABLAS AUTO, ESCUDERIA';
+        THROW 50000, @errorDescripcion, 1
+	END CATCH
+		
+END
+GO  
+
+IF OBJECT_ID('GDD_EXPRESS.migracion_pilotos', 'P') IS NOT NULL
+    DROP PROCEDURE GDD_EXPRESS.migracion_pilotos
+GO
+
+GO
+CREATE PROCEDURE GDD_EXPRESS.migracion_pilotos
+AS
+BEGIN
+	DECLARE c_pilotos CURSOR FOR
+		SELECT distinct m.PILOTO_NOMBRE, m.PILOTO_APELLIDO, m.PILOTO_NACIONALIDAD, m.PILOTO_FECHA_NACIMIENTO, m.AUTO_MODELO, m.AUTO_NUMERO FROM gd_esquema.Maestra m
+		
+
+	BEGIN TRY
+	BEGIN TRANSACTION
+
+		declare @pilo_nombre nvarchar(50)
+		declare @pilo_apellido nvarchar(50)
+		declare @pilo_nacionalidad nvarchar(50)
+		declare @pilo_fecha_nacimiento date
+		declare @auto_modelo nvarchar(255)
+		declare @auto_numero int
+
+		OPEN c_pilotos
+		FETCH NEXT FROM c_pilotos INTO @pilo_nombre, @pilo_apellido, @pilo_nacionalidad, @pilo_fecha_nacimiento, @auto_modelo, @auto_numero
+		WHILE (@@FETCH_STATUS = 0)
+		BEGIN
+			declare @id_pais int
+			declare @id_auto int
+
+			SET @id_pais = (select p.PAIS_ID from GDD_EXPRESS.Pais p WHERE p.PAIS_DETALLE = @pilo_nacionalidad)
+
+			SET @id_auto = (SELECT a.AUTO_ID FROM GDD_EXPRESS.Auto a
+								WHERE a.AUTO_MODELO = @auto_modelo and a.AUTO_NUMERO = @auto_numero)
+
+
+			INSERT INTO GDD_EXPRESS.Piloto (PILOTO_NOMBRE, PILOTO_APELLIDO, PILOTO_FECHA_NACIMIENTO, PILOTO_NACIONALIDAD, PILOTO_AUTO_ID)
+			VALUES (@pilo_nombre, @pilo_apellido, @pilo_fecha_nacimiento, @id_pais,  @id_auto)
+			
+
+			FETCH NEXT FROM c_pilotos INTO @pilo_nombre, @pilo_apellido, @pilo_nacionalidad, @pilo_fecha_nacimiento, @auto_modelo, @auto_numero
+
+		END
+
+		CLOSE c_pilotos  
+		DEALLOCATE c_pilotos 
+	COMMIT TRANSACTION	
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		DECLARE @errorDescripcion VARCHAR(255)
+		SELECT @errorDescripcion = ERROR_MESSAGE() + ' ERROR MIGRANDO DATOS EN TABLA PILOTO';
+        THROW 50001, @errorDescripcion, 1
+	END CATCH
+		
+END
+GO  
+
+IF OBJECT_ID('GDD_EXPRESS.migracion_circuitos_sectores', 'P') IS NOT NULL
+    DROP PROCEDURE GDD_EXPRESS.migracion_circuitos_sectores
+GO
+
+GO
+CREATE PROCEDURE GDD_EXPRESS.migracion_circuitos_sectores
+AS
+BEGIN
+	DECLARE c_escuderia CURSOR FOR
+		SELECT distinct ESCUDERIA_NOMBRE, ESCUDERIA_NACIONALIDAD FROM gd_esquema.Maestra
+		where ESCUDERIA_NOMBRE IS NOT NULL
+		
+	
+	CREATE TABLE #t_autos(
+		AUTO_MODELO nvarchar(255),
+		AUTO_NUMERO int,
+		ESCUDERIA_NOMBRE nvarchar(255)
+	)
+
+	BEGIN TRY
+	BEGIN TRANSACTION
+
+		INSERT INTO #t_autos
+			SELECT distinct m.AUTO_MODELO, m.AUTO_NUMERO, m.ESCUDERIA_NOMBRE FROM gd_esquema.Maestra m
+			where m.AUTO_MODELO IS NOT NULL
+
+		declare @escuderia_nombre nvarchar(255)
+		declare @escuderia_nacionalidad nvarchar(255)
+
+		OPEN c_escuderia
+		FETCH NEXT FROM c_escuderia INTO @escuderia_nombre, @escuderia_nacionalidad
+		WHILE (@@FETCH_STATUS = 0)
+		BEGIN
+			declare @id_pais int
+			declare @id_escuderia int
+			declare @id_auto int
+
+			SET @id_pais = (select p.PAIS_ID from GDD_EXPRESS.Pais p WHERE p.PAIS_DETALLE = @escuderia_nacionalidad)
+
+			INSERT INTO GDD_EXPRESS.Escuderia (ESCUDERIA_NOMBRE, ESCUDERIA_PAIS_ID)
+			VALUES (@escuderia_nombre, @id_pais)
+
+			SET @id_escuderia = @@IDENTITY
+
+			INSERT INTO GDD_EXPRESS.Auto (AUTO_MODELO, AUTO_NUMERO, AUTO_ESCUDERIA_ID)
+				SELECT ta.AUTO_MODELO, ta.AUTO_NUMERO, @id_escuderia FROM #t_autos ta
+				WHERE ta.ESCUDERIA_NOMBRE = (SELECT esc.ESCUDERIA_NOMBRE FROM GDD_EXPRESS.Escuderia esc
+				WHERE esc.ESCUDERIA_ID = @id_escuderia)
+
+			FETCH NEXT FROM c_escuderia INTO @escuderia_nombre, @escuderia_nacionalidad
+
+		END
+
+		CLOSE c_escuderia  
+		DEALLOCATE c_escuderia 
+	COMMIT TRANSACTION	
+	END TRY
+
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		DECLARE @errorDescripcion VARCHAR(255)
+		SELECT @errorDescripcion = ERROR_MESSAGE() + ' ERROR MIGRANDO DATOS EN TABLAS AUTO, ESCUDERIA';
+        THROW 50000, @errorDescripcion, 1
+	END CATCH
+		
+END
+GO  
+
+EXECUTE GDD_EXPRESS.migracion_parametros;
+EXECUTE GDD_EXPRESS.migracion_autos_escuderias;
+EXECUTE GDD_EXPRESS.migracion_pilotos;
+
+SELECT * FROM GDD_EXPRESS.Auto a
+inner join GDD_EXPRESS.Escuderia e on e.[ESCUDERIA_ID] = a.AUTO_ESCUDERIA_ID 
+
+SELECT * FROM GDD_EXPRESS.Piloto
+
+SELECT * FROM GDD_EXPRESS.Pais
